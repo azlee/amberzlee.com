bfdf8c722c1d6b3248f0b0d62936a6f2c000d993
Amber Lee <azlee@uwaterloo.ca>
2021-01-29 19:46:22 -0800

    Allow emojis in name input

--- cards/js/main.js
+++ cards/js/main.js
@@ -843,9 +843,8 @@ function addDisableEnableButton() {
   
     function validateNextButton() {
       var isNameEntered = $('#nameInput').val().trim() !== '';
-      var isNumericName = /^[a-zA-Z]*$/g.test($('#nameInput').val());
       var isRoomCodeEntered = !roomCodeButton || ($('#roomCodeInput').val().trim().length === 4);
-      $('#joinOrCreateGame').prop('disabled', !isNameEntered || !isRoomCodeEntered || !isNumericName);
+      $('#joinOrCreateGame').prop('disabled', !isNameEntered || !isRoomCodeEntered);
     }
   
     $('#nameInput').on('keyup', validateNextButton);
