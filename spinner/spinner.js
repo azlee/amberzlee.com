a463cbac45c9ca0208f7f278bf58c0795e7bdf8f
Amber Lee <amber_Lee1993@hotmail.com>
2022-08-06 15:37:42 -0700

    Add confetti

--- spinner/spinner.js
+++ spinner/spinner.js
@@ -277,6 +277,45 @@ function generateSpinner(teamMembers) {
         d3.select("#chosenPerson")[0][0].style.transform = "translate(0)";
         oldrotation = rotation;
         container.on("click", spin);
+        const count = 200;
+        const defaults = {
+          origin: { y: 0.7 },
+        };
+
+        function fire(particleRatio, opts) {
+          confetti(
+            Object.assign({}, defaults, opts, {
+              particleCount: Math.floor(count * particleRatio),
+            })
+          );
+        }
+
+        fire(0.25, {
+          spread: 26,
+          startVelocity: 55,
+        });
+        fire(0.2, {
+          spread: 60,
+        });
+        fire(0.35, {
+          spread: 100,
+          decay: 0.91,
+          scalar: 0.8,
+        });
+        fire(0.1, {
+          spread: 120,
+          startVelocity: 25,
+          decay: 0.92,
+          scalar: 1.2,
+        });
+        fire(0.1, {
+          spread: 150,
+          startVelocity: 45,
+        });
+        fire(0.4, {
+          spread: 200,
+          startVelocity: 55,
+        });
       });
   }
   //make arrow
